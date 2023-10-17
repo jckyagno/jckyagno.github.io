584da0ba3acbf0e2228e8bd6e3f5214cbcf9a115
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-10-17 08:50:45 -1000

    Initial commit

---
+++ js/portfolio_website.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.remove('fade-out');
+})
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function navContact() {
+    if (document.getElementById("myForm").style.display == "none") {
+        openForm();
+    } else {
+        closeForm();
+    }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "flex";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every two seconds
+}
